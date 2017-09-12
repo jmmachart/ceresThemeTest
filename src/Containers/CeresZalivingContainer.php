7ecddbfb8cdb36de5b040a418a4a221c1234aec2
Jan Mallwitz <mallwitz@machart-studios.de>
2017-09-12 10:57:30 +0200

    bugfix container, conf

--- src/Containers/CeresZalivingContainer.php
+++ src/Containers/CeresZalivingContainer.php
@@ -4,7 +4,7 @@ namespace CeresZaliving\Containers;
 
 use Plenty\Plugin\Templates\Twig;
 
-class ThemeContainer {
+class CeresZalivingContainer {
     # public function call(Twig $twig):string {
     public function call(Twig $twig) {
         return $twig->render('CeresZaliving::content.Theme');
